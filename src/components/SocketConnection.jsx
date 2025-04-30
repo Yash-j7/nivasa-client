e9acea3b964ea9511ba1c04b3e2b866ce95bdbcf
Yash Jha <yashjha00000@gmail.com>
2025-05-01 01:38:49 +0530

    deploying1

--- src/components/SocketConnection.jsx
+++ src/components/SocketConnection.jsx
@@ -29,7 +29,7 @@ const SocketConnection = () => {
     const loadPrevNotification = async () => {
       try {
         const unseenNotificaton = await fetch(
-          `http://localhost:3000/api/notification/${currentUser._id}`,
+          `api/notification/${currentUser._id}`,
           {
             credentials: "include", // This is crucial
           }
@@ -54,14 +54,11 @@ const SocketConnection = () => {
   //=========== Store notifications to DB =============//
   const sendNotificationToDB = async (notificationData) => {
     try {
-      const sendNotification = await fetch(
-        "http://localhost:3000/api/notification/create",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(notificationData),
-        }
-      );
+      const sendNotification = await fetch("api/notification/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(notificationData),
+      });
       const response = await sendNotification.json();
       if (response.success === false) {
         console.log(response);
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const apiBase = process.env.REACT_APP_SERVER_BASE_URL || "/api";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: apiBase,
         secure: false,
+        changeOrigin: true,
       },
     },
   },
-
   plugins: [react()],
 });
