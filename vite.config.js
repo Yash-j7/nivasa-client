6d60a48cc6b037edd3998d82d97d53b981995375
Yash Jha <yashjha00000@gmail.com>
2025-05-01 09:46:22 +0530

    deploying4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const apiBase = process.env.REACT_APP_SERVER_BASE_URL || "/api";
+const apiBase = "http://43.204.115.99:3000";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
