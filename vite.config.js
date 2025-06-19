2db5d6645fcc9320e1ff604a071f37a8c9371531
Yash Jha <yashjha00000@gmail.com>
2025-06-19 19:43:28 +0530

    url addded

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://nivasa.onrender.com",
         secure: false,
         changeOrigin: true,
         // No rewrite needed since backend expects /api prefix
@@ -16,7 +16,7 @@ export default defineConfig({
   plugins: [react()],
   define: {
     "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
-      "http://localhost:3000/api"
+      "https://nivasa.onrender.com/api"
     ),
   },
 });
